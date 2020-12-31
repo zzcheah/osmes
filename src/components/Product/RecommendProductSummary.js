62f22c684fbcac98155351ad2e370f1c26145dea
captainwong98 <cheecingwong@student.usm.my>
2020-12-31 23:09:46 +0800

    recommendation

---
+++ src/components/Product/RecommendProductSummary.js
@@ -0,0 +1,83 @@
+import React, {useState} from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { Card, CardContent, Grid, Typography } from "@material-ui/core";
+import CardMedia from '@material-ui/core/CardMedia';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableRow from "@material-ui/core/TableRow";
+import { Height } from '@material-ui/icons';
+
+
+
+const useStyles = makeStyles((theme) => ({
+    main: {
+      paddingTop: "20px",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    media: {
+        paddingRight: '100%',
+        width: 10,
+        height: 10,
+        paddingTop: '100%',
+      },
+      root: {
+        maxWidth: 360,
+        maxHeight: 700,
+        width: 155,
+        height: 290
+      },
+  }));
+
+  const BlueTextTypography = withStyles({
+    root: {
+      color: "#001eb3"
+    }
+  })(Typography);
+
+
+const RecommendProductSummary = ({products}) => {
+    const classes = useStyles();
+    //console.log(products);
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container component="main">
+                <div className={classes.main}>
+                    <Grid container spacing={1}>
+                        <Grid item xs={1} key={products.id}>
+                            <Card  className={classes.root}>
+                            <CardMedia
+                            className={classes.media}
+                            image= {products.images?products.images[0].url:null}
+                            title= {products.name}
+                            />
+                                <CardContent>
+                                    <div style={{height:"90px", align:"center"}}>
+                                        <Typography gutterBottom variant="h5" component="h2" align="center" style={{fontSize:"20px"}}>
+                                            {products.name}
+                                        </Typography>
+                                    </div>
+                                    <Typography
+                                        variant="body2"
+                                        color="textSecondary"
+                                        component="p"
+                                        >
+                                        <b>RM: {products.price} </b>
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </div>
+            </Container>
+        </React.Fragment>
+    )
+}
+
+export default RecommendProductSummary
---
+++ src/components/Product/Recommendation.js
@@ -0,0 +1,109 @@
+import { Divider } from "@material-ui/core";
+//import React, {useEffect, useState} from 'react'
+import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ProductList from "./ProductList";
+import RecommendProductSummary from "./RecommendProductSummary";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import SearchBar from "../SearchBar";
+//import firebase from "./configs/firebaseConfig";
+import firebase from "../../configs/firebaseConfig";
+import { Link } from "react-router-dom";
+import { Card, CardContent, Grid, Typography } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import { useSelector } from "react-redux";
+import { useFirestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    tabs: {
+      padding: "30px 0px",
+      flexGrow: 1,
+      borderRight: `1px solid ${theme.palette.divider}`,
+      maxWidth: "250px",
+    },
+    paper: {
+        height: 140,
+        width: 100,
+      },
+      control: {
+        padding: theme.spacing(2),
+      },
+  }));
+  
+export default function Recommendation() {
+
+    const x = "Thermometer"
+    const y = "Medicine"
+    const classes = useStyles();
+    const auth = useSelector((state) => state.firebase.profile);
+    const lastView = auth.lastView;
+    console.log("lastView",lastView)
+
+    useFirestoreConnect([
+        {
+          collection: "products",
+          where: [["category", "==", lastView]],
+          storeAs: "recommendProducts",
+          limit: "3"
+        },
+        {
+            collection: "products",
+            where: [["category", "==", y]],
+            storeAs: "recommendProducts2",
+            limit: "3"
+        },
+    ]);
+
+    const recommendProducts = useSelector((state) => state.firestore.ordered.recommendProducts);
+    // const recommendProducts2 = useSelector((state) => state.firestore.ordered.recommendProducts2);
+
+    return (
+        <div>
+            <div
+                style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                }}
+            >
+                <h2>Recommendations</h2>
+            </div>
+            {/* <Container component="main"> */}
+            <Grid item xs={12}>                    
+                    <Grid container justify="center" spacing={0}>
+                        {recommendProducts &&
+                            recommendProducts.map((products) => {
+                                return (
+                                    <Link key={products.id} to={"/product/" + products.id} style={{ textDecoration: 'none' }}>
+                                    <RecommendProductSummary
+                                        products={products}
+                                        key={products.id}
+                                    />
+                                    </Link>
+                                );
+                            })
+                        }
+                        {/* {recommendProducts2 &&
+                            recommendProducts2.map((products) => {
+                                return (
+                                    <Link key={products.id} to={"/product/" + products.id} style={{ textDecoration: 'none' }}>
+                                    <RecommendProductSummary
+                                        products={products}
+                                        key={products.id}
+                                    />
+                                    </Link>
+                                );
+                            })
+                        } */}
+                    </Grid>
+                </Grid>
+            {/* </Container> */}
+      </div>
+    );
+}
